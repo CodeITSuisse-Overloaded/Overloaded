22dcd17f144ea6dff50c09940516e31562d3c7c3
angkoonhwee <akoonhwee@gmail.com>
2022-09-18 01:14:03 +0800

    fix: rubiks

--- src/main/java/com/overloaded/overloaded/rubriks/Cube.java
+++ src/main/java/com/overloaded/overloaded/rubriks/Cube.java
@@ -8,8 +8,8 @@ public class Cube {
 
     public void parseOps() {
         for (int i = 0; i < ops.length(); i++) {
-            boolean isClockwise = (i + 1) < ops.length() && ops.charAt(i + 1) != 'i';
-//            System.out.println("Step: " + ops.charAt(i) + ", clockwise: " + isClockwise);
+            boolean isClockwise = ((i + 1) < ops.length() && ops.charAt(i + 1) != 'i') || i + 1 == ops.length();
+            System.out.println("Step: " + ops.charAt(i) + ", clockwise: " + isClockwise);
             switch (ops.charAt(i)) {
                 case 'U':
                 case 'D':
@@ -24,10 +24,10 @@ public class Cube {
                     state.frontBackAction(ops.charAt(i), isClockwise);
                     break;
             }
-            if (isClockwise) {
+            if (!isClockwise) {
                 i++;
             }
-//            System.out.println(state.toString());
+            System.out.println(state.toString());
         }
     }
 
@@ -55,6 +55,17 @@ public class Cube {
         int[][] r;
         int[][] b;
         int[][] d;
+
+        int[][] copyArray(int[][] ori) {
+            int[][] res = new int[3][3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    res[i][j] = ori[i][j];
+                }
+            }
+
+            return res;
+        }
 
         public void setU(int[][] u) {
             this.u = u;
@@ -110,17 +121,17 @@ public class Cube {
         // down = all bottom rows affected --> x[2]
         int rowAffected = face == 'U' ? 0 : 2;
         // copy all required arrays to ensure immutability
-        int[][] cLeft = l.clone();
-        int[][] cFront = f.clone();
-        int[][] cRight = r.clone();
-        int[][] cBack = b.clone();
-        if (!isClockwise) {
+        int[][] cLeft = copyArray(l);
+        int[][] cFront = copyArray(f);
+        int[][] cRight = copyArray(r);
+        int[][] cBack = copyArray(b);
+        if ((face == 'U' && !isClockwise) || (face == 'D' && isClockwise)) {
             f[rowAffected] = cLeft[rowAffected];
             r[rowAffected] = cFront[rowAffected];
             b[rowAffected] = cRight[rowAffected];
             l[rowAffected] = cBack[rowAffected];
-        } else {
-            // anticlockwise --> [l->b, b->r, r->f, f->l]
+        } else if ((face == 'U' && isClockwise) || (face == 'D' && !isClockwise)) {
+            // clockwise --> [l->b, b->r, r->f, f->l]
             b[rowAffected] = cLeft[rowAffected];
             r[rowAffected] = cBack[rowAffected];
             f[rowAffected] = cRight[rowAffected];
@@ -132,20 +143,21 @@ public class Cube {
             // affected faces for clockwise --> [f->u, u->b, b->d, d->f]
             // rows&cols affected --> x[0][2], x[1][2], x[2][2]
             int colAffected = face == 'R' ? 2 : 0;
-            int[][] cFront = f.clone();
-            int[][] cUp = u.clone();
-            int[][] cBack = b.clone();
-            int[][] cDown = d.clone();
-            if (isClockwise) {
-                for (int i = 0; i < 2; i++) {
+            int[][] cFront = copyArray(f);
+            int[][] cUp = copyArray(u);
+            int[][] cBack = copyArray(b);
+            int[][] cDown = copyArray(d);
+            if ((face == 'R' && isClockwise) || (face == 'L' && !isClockwise)) {
+                for (int i = 0; i < 3; i++) {
                     u[i][colAffected] = cFront[i][colAffected];
                     b[i][colAffected] = cUp[i][colAffected];
                     d[i][colAffected] = cBack[i][colAffected];
                     f[i][colAffected] = cDown[i][colAffected];
+                    System.out.println(Arrays.deepToString(cDown));
                 }
-            } else {
+            } else if ((face == 'R' && !isClockwise) || (face == 'L' && isClockwise)) {
                 // anticlockwise --> [u->f, f->d, d->b, b->u]
-                for (int i = 0; i < 2; i++) {
+                for (int i = 0; i < 3; i++) {
                     f[i][colAffected] = cUp[i][colAffected];
                     d[i][colAffected] = cFront[i][colAffected];
                     b[i][colAffected] = cDown[i][colAffected];
@@ -158,20 +170,20 @@ public class Cube {
             // affected faces for clockwise --> [l->u, u->r, r->d, d->l]
             // rows&col affected --> x[2][0], x[2][1], x[2][2]
             int rowAffected = face == 'F' ? 2 : 0;
-            int[][] cLeft = l.clone();
-            int[][] cUp = u.clone();
-            int[][] cRight = r.clone();
-            int[][] cDown = d.clone();
-            if (isClockwise) {
-                for (int i = 0; i < 2; i++) {
+            int[][] cLeft = copyArray(l);
+            int[][] cUp = copyArray(u);
+            int[][] cRight = copyArray(r);
+            int[][] cDown = copyArray(d);
+            if ((face == 'F' && isClockwise) || (face == 'B' && !isClockwise)) {
+                for (int i = 0; i < 3; i++) {
                     u[rowAffected][i] = cLeft[rowAffected][i];
                     r[rowAffected][i] = cUp[rowAffected][i];
                     d[rowAffected][i] = cRight[rowAffected][i];
                     l[rowAffected][i] = cDown[rowAffected][i];
                 }
-            } else {
+            } else if ((face == 'F' && !isClockwise) || (face == 'B' && isClockwise)) {
                 // anticlockwise --> [l->d, d->r, r->u, u->l]
-                for (int i = 0; i < 2; i++) {
+                for (int i = 0; i < 3; i++) {
                     d[rowAffected][i] = cLeft[rowAffected][i];
                     r[rowAffected][i] = cDown[rowAffected][i];
                     u[rowAffected][i] = cRight[rowAffected][i];
