0a493ddf3167c7c868fd69a604c09c1c020b5a7c
zsb258 <zhuang.shengbin@u.nus.edu>
2022-09-17 18:05:31 +0800

    fix: CalendarDays part2

--- src/test/java/com/overloaded/overloaded/TestCalendarDays.java
+++ src/test/java/com/overloaded/overloaded/TestCalendarDays.java
@@ -44,13 +44,38 @@ public class TestCalendarDays {
         String expected = "mtw f  , tw  s ,mt tf  ,m  tfs ,m   fs ,   tf s,m  tfss,    fs ,m  t s ,     s ,    fs , tw fss,";
         int year = 2094;
         List<Integer> days = List.of(
-                4,5,6,1,33,34,37,60,61,63,64,95,91,92,93,123,127,121,154,155,157,186,182,183,184,185,218,219,249,245,247,275,309,310,341,335,337,338,339);
+                4,5,6,1,33,34,37,60,61,63,64,95,91,92,93,123,127,121,154,155,157,186,182,183,184,185,218,219,249,245,247,275,309,310,341,335,337,338,339
+        );
         Assertions.assertEquals(expected, CalendarDays.calculatePartOne(year, days));
+    }
+
+    @Test
+    public void testCalculatePartTwo1() {
+        String expected = "mtw f  , tw  s ,mt tf  ,m  tfs ,m   fs ,   tf s,m  tfss,    fs ,m  t s ,     s ,    fs , tw fss,";
+        CalendarDays.Input input = new CalendarDays.Input();
+        input.numbers = CalendarDays.partTwo(expected);
+        String calculated = CalendarDays.partOne(input);
+        Assertions.assertEquals(expected, calculated);
+    }
+
+    @Test
+    public void testCalculatePartTwo2() {
+        String expected = "weekend,weekday, t     ,alldays,alldays,alldays,alldays,alldays,alldays,alldays,alldays,       ,";
+        CalendarDays.Input input = new CalendarDays.Input();
+        input.numbers = CalendarDays.partTwo(expected);
+        String calculated = CalendarDays.partOne(input);
+        Assertions.assertEquals(expected, calculated);
     }
 
 
     @Test
     public void working() {
-
+//        GregorianCalendar cal = new GregorianCalendar(2022, Calendar.SEPTEMBER, 17);
+//        System.out.println(cal.getFirstDayOfWeek()==Calendar.SUNDAY);
+//        System.out.println(cal.get(Calendar.DAY_OF_YEAR));
+//        cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+//        System.out.println(cal.get(Calendar.DAY_OF_WEEK));
+//        System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+//        System.out.println(cal.get(Calendar.DAY_OF_YEAR));
     }
 }
